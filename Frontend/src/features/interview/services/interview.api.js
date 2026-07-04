991c1237eceec5a3c8068a1dd31977f684771fff
raviraj-547 <ravirajsamota17@gmail.com>
2026-07-04 17:22:44 +0530

    intial commit

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
-})
+});
+
+export default api;
 
 
 /**
